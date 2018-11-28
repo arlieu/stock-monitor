d49248750dbe3a70e5cb422e961ad40a1b2f763f
Avery Lieu <avelieu@gmail.com>
2018-11-28 14:23:37 -0800

    automate stock-monitor run

--- schtasks.sh
+++ schtasks.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo schtasks /run /tn "python stock_graphql_api/manage.py makemigrations &&
-    python stock_graphql_api/manage.py migrate && python stock_graphql_api/manage.py runserver"
+echo python stock_graphql_api/manage.py makemigrations &&
+    python stock_graphql_api/manage.py migrate && python stock_graphql_api/manage.py runserver
 
 echo schtasks /create
     /tn scrape-and-load
-    /tr "python /crawler/stock_crawler.py && python stock_graphql_api/manage.py loaddata ../resources/output.json"
+    /tr "python /crawler/stock_crawler.py && python stock_graphql_api/manage.py loaddata /resources/output.json"
     /sc daily
     /st 09:30 /ri 30 /et16:00
 
